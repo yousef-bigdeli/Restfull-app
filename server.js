37094c929dca1c1dedbd73d546e883b91ff4665d
yousef-bigdeli <yousefbigdeli96@gmail.com>
2022-08-11 15:51:08 +0430

    Add helmet middleware

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const conn = require("./models/connection"); // mongodb connection
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 
 app.use(express.json());
+app.use(helmet());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
